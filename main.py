74caab727ef054d056dfed08657e2e0b00d7c9a4
ProTrickstar <2007swastiksingh@gmail.com>
2021-07-21 15:33:06 +0530

    ProTrickstar

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Swathi K")
+number = 14
+print(number)
+myFriendList = ["Shubham", "Vinayak", "Dhanish"]
+print(myFriendList, "Vinayak")
+print(myFriendList[1])
+pocketMoney = input("Enter The Pocket Money You Get: ")
+print(pocketMoney)
+age = int(input("Enter Your Age: "))
+if(age < 15):
+    print("You Are A Teenager")
+elif(age > 18):
+    print("You Are An Adult")
+else:
+    print("You Are Unkown")
+myFriends = ["Friend1", "Friend2", "Friend3"] 
+for friend in myFriends: 
+    print("friend")
+count = 100
+while(count >= 50):
+    print(count)
+    count = count-10
